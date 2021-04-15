14b39c0a0bcb41eb9a2e5ed4a716925e8e7c86eb
Yevhenii Pazii <epaziy@softserveinc.com>
2021-04-15 14:42:03 +0300

    Initial commit

---
+++ src/main/java/com/example/fhirtest/FhirTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.fhirtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FhirTestApplication.class, args);
+    }
+
+}
